06ddc4c28aeffc41b91ff5f0905dd8a39e237c73
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-05-02 16:48:21 -0300

    feat: Added Loading component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
 import { getStories } from "../utils/api";
 
 export default function Home() {
 	const [stories, setStories] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
+		setLoading(true);
 		handleInitialStories();
 	}, []);
 
 	return (
-		<div>
-			Home
-			<ul>
+		<div className="home__container">
+			{loading && <Loading />}
+			<ul className="stories__list">
 				{Array.isArray(stories) &&
-					stories.map((story) => <li key={story.id}>{story.title}</li>)}
+					!loading &&
+					stories.map((story) => (
+						<li key={story.id} className="stories__item">
+							<a href={story.url} className="stories__link">
+								{story.title}
+							</a>
+						</li>
+					))}
 			</ul>
 		</div>
 	);
 
 	function handleInitialStories() {
-		getStories("top").then((response) => setStories(response));
+		getStories("top").then((response) => {
+			setStories(response);
+			setLoading(false);
+		});
 	}
 }
