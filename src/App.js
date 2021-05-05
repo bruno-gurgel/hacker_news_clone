2514a2e39a0888316ddbc2d23777384c5c369b3c
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-05-05 11:50:29 -0300

    feat: Added Navigation component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 
 function App() {
 	return (
 		<div className="App">
+			<Navigation />
 			<Home />
 		</div>
 	);
