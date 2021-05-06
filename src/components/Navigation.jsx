046810ad210d313bee9c5b02a64e7521e791f8d8
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-05-06 14:18:32 -0300

    chore: Removed unused onClick function

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
-	const [active, setActive] = useState("top");
 	return (
 		<React.Fragment>
 			<nav className="nav">
-				<NavLink
-					exact
-					to="/"
-					activeClassName="active"
-					className="nav__link"
-					onClick={() => setActive("top")}
-				>
+				<NavLink exact to="/" activeClassName="active" className="nav__link">
 					Top
 				</NavLink>
-				<NavLink
-					to="/new"
-					activeClassName="active"
-					className="nav__link"
-					onClick={() => setActive("new")}
-				>
+				<NavLink to="/new" activeClassName="active" className="nav__link">
 					New
 				</NavLink>
 			</nav>
