944502a979fee857a16f252918a75d96b7842c7c
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-05-06 14:18:12 -0300

    feat: Added Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 
-function App() {
+function App(props) {
+	console.log(props);
 	return (
 		<div className="App">
-			<Navigation />
-			<Home />
+			<Route path="/" component={Navigation} />
+
+			<Switch>
+				<Route path="/new" render={(props) => <Home {...props} storyType="new" />} />
+				<Route path="/" render={(props) => <Home {...props} storyType="top" />} />
+			</Switch>
 		</div>
 	);
 }
